5b88967c8e5b1faf50a4bde0ed05603c6745245a
King Wilder <king@gizmobeach.com>
2018-07-16 22:32:44 -0700

    Added more functionality to make the drop downs dynamic.

--- DynamicFormArray/src/app/customer/customer.component.ts
+++ DynamicFormArray/src/app/customer/customer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { SearchCriteria } from '../shared/models';
+import { labelArrayData, typeGroup } from '../shared/form-config';
 
 @Component({
   selector: 'app-customer',
@@ -8,6 +10,10 @@ import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 })
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
+  results: any[];
+  criteriaList: SearchCriteria[];
+  labelArray = labelArrayData;
+  supportedTypes: any[] = [];
 
   constructor(private fb: FormBuilder) { }
 
@@ -32,7 +38,8 @@ export class CustomerComponent implements OnInit {
         this.createItem({ labelName: 'FirstName', fieldValue: 'John', operation: 'EqualTo', dataType: 'String', inputType: 'text'}),
         this.createItem({
           labelName: 'StartDate',
-          fieldValue: new Date('2018-04-29T15:26:32'), operation: 'LessThan', dataType: 'DateTimeOffset', inputType: 'date'}),
+          fieldValue: new Date('2018-04-29T15:26:32').toISOString().substring(0, 10),
+          operation: 'LessThan', dataType: 'DateTimeOffset', inputType: 'date'}),
         this.createItem({
           labelName: 'ContributionAmount', fieldValue: '123.45', operation: 'GreaterThan', dataType: 'Decimal', inputType: 'number'})
       ])
@@ -41,6 +48,8 @@ export class CustomerComponent implements OnInit {
 
   save() {
     console.log(this.customerForm.value);
+    this.results = this.customerForm.value;
+    this.mapToCriteria(this.results);
   }
 
   addItem() {
@@ -51,5 +60,38 @@ export class CustomerComponent implements OnInit {
   removeItem(index: number) {
     const items = <FormArray>this.customerForm.get('items');
     items.removeAt(index);
+  }
+
+  onLabelChange(value: any, index: number) {
+    // console.log('selected value: ', value);
+    const labelObj = labelArrayData.find(item => item.value === value);
+    // console.log('selected object: ', labelObj);
+    this.getSelectedOperations(labelObj.dataType, index);
+  }
+
+  mapToCriteria(formData: any) {
+    const criteriaList: SearchCriteria[] = [];
+    formData['items'].forEach(element => {
+      const criteria = new SearchCriteria();
+      criteria.FieldName = element.labelName;
+      criteria.FieldValue = element.fieldValue;
+      criteria.OperationName = element.operation;
+      criteria.Type = element.dataType;
+      criteria.Connector = 'Or';
+      criteriaList.push(criteria);
+    });
+    this.criteriaList = criteriaList;
+  }
+
+  getSelectedOperations(type: any, index: number) {
+    const typegroup = typeGroup.find(item => item.type === type);
+    console.log('typegroup: ', typegroup);
+    this.loadSupportedTypes(typegroup, index);
+  }
+
+  loadSupportedTypes(typegroup: any, index: number) {
+    const control = this.customerForm.get('items')['controls'][index];
+console.log('control: ', control);
+    this.supportedTypes = typegroup.supportedTypes;
   }
 }
--- DynamicFormArray/src/app/shared/form-config.ts
+++ DynamicFormArray/src/app/shared/form-config.ts
@@ -1,4 +1,61 @@
 import { FormDataItem } from './models';
 
-export const config: FormDataItem[] = [
+export const config: FormDataItem[] = [];
+
+export const labelArrayData: any[] = [
+  { value: 'Phone', text: 'Phone', dataType: 'text' },
+  { value: 'FirstName', text: 'First Name', dataType: 'text' },
+  { value: 'LastName', text: 'Last Name', dataType: 'text' },
+  {
+    value: 'ContributionAmount',
+    text: 'Contribution Amount',
+    dataType: 'number'
+  },
+  { value: 'StartDate', text: 'Start Date', dataType: 'date' }
+];
+
+export const typeGroup: any[] = [
+  { type: 'default', supportedTypes: ['EqualTo', 'NotEqualTo'] },
+  {
+    type: 'text',
+    supportedTypes: [
+      'Contains',
+      'EndsWith',
+      'EqualTo',
+      'IsEmpty',
+      'IsNotEmpty',
+      'IsNotNull',
+      'IsNotNullNorWhiteSpace',
+      'IsNull',
+      'IsNullOrWhiteSpace',
+      'NotEqualTo',
+      'StartsWith'
+    ]
+  },
+  {
+    type: 'number',
+    supportedTypes: [
+      'Between',
+      'EqualTo',
+      'GreaterThan',
+      'GreaterThanOrEqualTo',
+      'LessThan',
+      'LessThanOrEqualTo',
+      'NotEqualTo'
+    ]
+  },
+  { type: 'boolean', supportedTypes: ['EqualTo', 'NotEqualTo'] },
+  {
+    type: 'date',
+    supportedTypes: [
+      'Between',
+      'EqualTo',
+      'GreaterThan',
+      'GreaterThanOrEqualTo',
+      'LessThan',
+      'LessThanOrEqualTo',
+      'NotEqualTo'
+    ]
+  },
+  { type: 'nullable', supportedTypes: ['IsNotNull', 'IsNull'] }
 ];
